9abffbc52a4fc676f6b59c29644a2a20353f337d
Keith Tan <skeithtan@gmail.com>
2017-11-04 07:20:18 +0800

    Removed unecessary dist files

--- dist/js/tabs/Institutions/tabs/institution_tabs_list.js
+++ dist/js/tabs/Institutions/tabs/institution_tabs_list.js
@@ -12,10 +12,6 @@ var _overview = require("./overview");
 
 var _overview2 = _interopRequireDefault(_overview);
 
-var _programs = require("./programs");
-
-var _programs2 = _interopRequireDefault(_programs);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var tabs = [{
--- js/tabs/Institutions/tabs/institution_tabs_list.js
+++ js/tabs/Institutions/tabs/institution_tabs_list.js
@@ -1,6 +1,5 @@
 import React from "react";
 import InstitutionOverview from "./overview";
-import InstitutionPrograms from "./programs";
 
 const tabs = [
     {
