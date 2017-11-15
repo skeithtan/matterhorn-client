e726787f074db7d0707643fdea7a861595fffd49
Keith Tan <skeithtan@gmail.com>
2017-11-15 16:55:49 +0800

    Added tabs to archive_tabs_list

--- dist/js/tabs/Archives/tabs/archives_tabs_list.js
+++ dist/js/tabs/Archives/tabs/archives_tabs_list.js
@@ -1,9 +1,24 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
-var tabs = [];
+var tabs = [{
+    name: "Memorandums",
+    tab: undefined,
+    image: "./images/memorandumgrey.png",
+    activeImage: "./images/memorandumgreen.png"
+}, {
+    name: "Programs",
+    tab: undefined,
+    image: "./images/programsgrey.png",
+    activeImage: "./images/programsgreen.png"
+}, {
+    name: "Students",
+    tab: undefined,
+    image: "./images/studentgrey.png",
+    activeImage: "./images/studentgreen.png"
+}];
 
 exports.default = tabs;
 //# sourceMappingURL=archives_tabs_list.js.map
--- js/tabs/Archives/tabs/archives_tabs_list.js
+++ js/tabs/Archives/tabs/archives_tabs_list.js
@@ -1,3 +1,22 @@
-const tabs = [];
+const tabs = [
+    {
+        name : "Memorandums",
+        tab : undefined,
+        image : "./images/memorandumgrey.png",
+        activeImage : "./images/memorandumgreen.png",
+    },
+    {
+        name : "Programs",
+        tab : undefined,
+        image : "./images/programsgrey.png",
+        activeImage : "./images/programsgreen.png",
+    },
+    {
+        name : "Students",
+        tab : undefined,
+        image : "./images/studentgrey.png",
+        activeImage : "./images/studentgreen.png",
+    },
+];
 
 export default tabs;
