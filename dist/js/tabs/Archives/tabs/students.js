b7c8111eafd02375042ddeb836c9b79105dd8871
Keith Tan <skeithtan@gmail.com>
2017-11-16 16:00:56 +0800

    Added StudentArchives

---
+++ dist/js/tabs/Archives/tabs/students.js
@@ -0,0 +1,269 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _moment = require("moment");
+
+var _moment2 = _interopRequireDefault(_moment);
+
+var _graphql = require("../../../graphql");
+
+var _graphql2 = _interopRequireDefault(_graphql);
+
+var _archive_head = require("../archive_head");
+
+var _archive_head2 = _interopRequireDefault(_archive_head);
+
+var _loading = require("../../../components/loading");
+
+var _loading2 = _interopRequireDefault(_loading);
+
+var _reactstrap = require("reactstrap");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function fetchStudents(year, onResult) {
+    _graphql2.default.query("\n    {\n      students(archived: true, year_archived: " + year + ") {\n        id\n        category\n        id_number\n        college\n        family_name\n        first_name\n        middle_name\n        nickname\n        nationality\n        birth_date\n        sex\n        archived_at\n        archiver\n        institution {\n          name\n        }\n      }\n    }\n    ").then(onResult);
+}
+
+var StudentArchives = function (_Component) {
+    _inherits(StudentArchives, _Component);
+
+    function StudentArchives(props) {
+        _classCallCheck(this, StudentArchives);
+
+        var _this = _possibleConstructorReturn(this, (StudentArchives.__proto__ || Object.getPrototypeOf(StudentArchives)).call(this, props));
+
+        _this.state = {
+            activeYear: (0, _moment2.default)().year(),
+            students: null,
+            activeStudentId: null
+        };
+
+        _this.setActiveYear = _this.setActiveYear.bind(_this);
+        _this.setActiveStudent = _this.setActiveStudent.bind(_this);
+
+        fetchStudents(_this.state.activeYear, function (result) {
+            _this.setState({
+                students: result.students
+            });
+        });
+        return _this;
+    }
+
+    _createClass(StudentArchives, [{
+        key: "setActiveStudent",
+        value: function setActiveStudent(student) {
+            this.setState({
+                activeStudentId: student.id
+            });
+
+            //TODO: setSidebarContent
+        }
+    }, {
+        key: "setActiveYear",
+        value: function setActiveYear(year) {
+            var _this2 = this;
+
+            this.setState({
+                activeYear: year,
+                activeStudentId: null,
+                students: null //loading
+            });
+
+            this.props.setSidebarContent(null);
+
+            fetchStudents(this.state.activeYear, function (result) {
+                _this2.setState({
+                    students: result.students
+                });
+            });
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            return _react2.default.createElement(
+                "div",
+                { className: "d-flex flex-column h-100" },
+                _react2.default.createElement(
+                    _archive_head2.default,
+                    { setActiveYear: this.setActiveYear,
+                        activeYear: this.state.activeYear },
+                    "Student Archives"
+                ),
+                _react2.default.createElement(StudentArchivesTable, { students: this.state.students,
+                    activeYear: this.state.activeYear,
+                    setSidebarContent: this.props.setSidebarContent,
+                    activeStudentId: this.state.activeStudentId,
+                    setActiveStudent: this.setActiveStudent })
+            );
+        }
+    }]);
+
+    return StudentArchives;
+}(_react.Component);
+
+var StudentArchivesTable = function (_Component2) {
+    _inherits(StudentArchivesTable, _Component2);
+
+    function StudentArchivesTable(props) {
+        _classCallCheck(this, StudentArchivesTable);
+
+        var _this3 = _possibleConstructorReturn(this, (StudentArchivesTable.__proto__ || Object.getPrototypeOf(StudentArchivesTable)).call(this, props));
+
+        _this3.emptyState = _this3.emptyState.bind(_this3);
+        return _this3;
+    }
+
+    _createClass(StudentArchivesTable, [{
+        key: "emptyState",
+        value: function emptyState() {
+            return _react2.default.createElement(
+                "div",
+                { className: "loading-container" },
+                _react2.default.createElement(
+                    "h3",
+                    null,
+                    "There were no archived students in ",
+                    this.props.activeYear,
+                    "."
+                )
+            );
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            var _this4 = this;
+
+            if (this.props.students === null) {
+                return _react2.default.createElement(_loading2.default, null);
+            }
+
+            if (this.props.students.length === 0) {
+                return this.emptyState();
+            }
+
+            var rows = this.props.students.map(function (student) {
+                return _react2.default.createElement(StudentArchivesRow, { student: student,
+                    key: student.id,
+                    isActive: _this4.props.activeStudentId === student.id,
+                    onClick: function onClick() {
+                        return _this4.props.setActiveStudent(student);
+                    } });
+            });
+
+            return _react2.default.createElement(
+                _reactstrap.Table,
+                { striped: true,
+                    hover: true },
+                _react2.default.createElement(
+                    "thead",
+                    null,
+                    _react2.default.createElement(
+                        "tr",
+                        null,
+                        _react2.default.createElement(
+                            "th",
+                            null,
+                            "ID Number"
+                        ),
+                        _react2.default.createElement(
+                            "th",
+                            null,
+                            "Name"
+                        ),
+                        _react2.default.createElement(
+                            "th",
+                            null,
+                            "Archive Date"
+                        ),
+                        _react2.default.createElement(
+                            "th",
+                            null,
+                            "Archived By"
+                        )
+                    )
+                ),
+                _react2.default.createElement(
+                    "tbody",
+                    null,
+                    rows
+                )
+            );
+        }
+    }]);
+
+    return StudentArchivesTable;
+}(_react.Component);
+
+var StudentArchivesRow = function (_Component3) {
+    _inherits(StudentArchivesRow, _Component3);
+
+    function StudentArchivesRow(props) {
+        _classCallCheck(this, StudentArchivesRow);
+
+        return _possibleConstructorReturn(this, (StudentArchivesRow.__proto__ || Object.getPrototypeOf(StudentArchivesRow)).call(this, props));
+    }
+
+    _createClass(StudentArchivesRow, [{
+        key: "render",
+        value: function render() {
+            var student = this.props.student;
+            var archiveDate = (0, _moment2.default)(student.archived_at).format("LLL");
+            var className = this.props.isActive ? "bg-dlsu-lighter text-white" : null;
+
+            return _react2.default.createElement(
+                "tr",
+                { className: className,
+                    onClick: this.props.onClick },
+                _react2.default.createElement(
+                    "td",
+                    null,
+                    student.id_number
+                ),
+                _react2.default.createElement(
+                    "td",
+                    null,
+                    _react2.default.createElement(
+                        "b",
+                        null,
+                        student.family_name
+                    ),
+                    ", ",
+                    student.first_name,
+                    " ",
+                    student.middle_name
+                ),
+                _react2.default.createElement(
+                    "td",
+                    null,
+                    archiveDate
+                ),
+                _react2.default.createElement(
+                    "td",
+                    null,
+                    student.archiver
+                )
+            );
+        }
+    }]);
+
+    return StudentArchivesRow;
+}(_react.Component);
+
+exports.default = StudentArchives;
+//# sourceMappingURL=students.js.map
---
+++ js/tabs/Archives/tabs/students.js
@@ -0,0 +1,169 @@
+import React, { Component } from "react";
+import moment from "moment";
+import graphql from "../../../graphql";
+import ArchivesHead from "../archive_head";
+import LoadingSpinner from "../../../components/loading";
+import { Table } from "reactstrap";
+
+
+function fetchStudents(year, onResult) {
+    graphql.query(`
+    {
+      students(archived: true, year_archived: ${year}) {
+        id
+        category
+        id_number
+        college
+        family_name
+        first_name
+        middle_name
+        nickname
+        nationality
+        birth_date
+        sex
+        archived_at
+        archiver
+        institution {
+          name
+        }
+      }
+    }
+    `).then(onResult);
+}
+
+class StudentArchives extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeYear : moment().year(),
+            students : null,
+            activeStudentId : null,
+        };
+
+        this.setActiveYear = this.setActiveYear.bind(this);
+        this.setActiveStudent = this.setActiveStudent.bind(this);
+
+        fetchStudents(this.state.activeYear, result => {
+            this.setState({
+                students : result.students,
+            });
+        });
+    }
+
+    setActiveStudent(student) {
+        this.setState({
+            activeStudentId : student.id,
+        });
+
+
+        //TODO: setSidebarContent
+    }
+
+    setActiveYear(year) {
+        this.setState({
+            activeYear : year,
+            activeStudentId : null,
+            students : null //loading
+        });
+
+        this.props.setSidebarContent(null);
+
+        fetchStudents(this.state.activeYear, result => {
+            this.setState({
+                students : result.students,
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div className="d-flex flex-column h-100">
+                <ArchivesHead setActiveYear={this.setActiveYear}
+                              activeYear={this.state.activeYear}>Student Archives</ArchivesHead>
+                <StudentArchivesTable students={this.state.students}
+                                      activeYear={this.state.activeYear}
+                                      setSidebarContent={this.props.setSidebarContent}
+                                      activeStudentId={this.state.activeStudentId}
+                                      setActiveStudent={this.setActiveStudent}/>
+            </div>
+        );
+    }
+}
+
+class StudentArchivesTable extends Component {
+    constructor(props) {
+        super(props);
+
+        this.emptyState = this.emptyState.bind(this);
+    }
+
+    emptyState() {
+        return (
+            <div className="loading-container">
+                <h3>There were no archived students in {this.props.activeYear}.</h3>
+            </div>
+        );
+    }
+
+    render() {
+        if (this.props.students === null) {
+            return <LoadingSpinner/>;
+        }
+
+        if (this.props.students.length === 0) {
+            return this.emptyState();
+        }
+
+        const rows = this.props.students.map(student => {
+            return <StudentArchivesRow student={student}
+                                       key={student.id}
+                                       isActive={this.props.activeStudentId === student.id}
+                                       onClick={() => this.props.setActiveStudent(student)}/>;
+        });
+
+        return (
+            <Table striped
+                   hover>
+                <thead>
+                <tr>
+                    <th>ID Number</th>
+                    <th>Name</th>
+                    <th>Archive Date</th>
+                    <th>Archived By</th>
+                </tr>
+                </thead>
+                <tbody>
+                {rows}
+                </tbody>
+            </Table>
+        );
+    }
+}
+
+class StudentArchivesRow extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const student = this.props.student;
+        const archiveDate = moment(student.archived_at).format("LLL");
+        const className = this.props.isActive ? "bg-dlsu-lighter text-white" : null;
+
+
+        return (
+            <tr className={className}
+                onClick={this.props.onClick}>
+                <td>{student.id_number}</td>
+                <td>
+                    <b>{student.family_name}</b>, {student.first_name} {student.middle_name}
+                </td>
+                <td>{archiveDate}</td>
+                <td>{student.archiver}</td>
+            </tr>
+        );
+    }
+}
+
+export default StudentArchives;
