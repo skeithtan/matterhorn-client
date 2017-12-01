c291265fbaf1bd74657da558998cda0b7f739e79
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-12-01 13:55:36 +0800

    Removed trailing comma in package.json

---
+++ dist/dist/js/dismissable_toast_maker.js
@@ -0,0 +1,40 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _izitoast = require("izitoast");
+
+var _izitoast2 = _interopRequireDefault(_izitoast);
+
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : { default: obj };
+}
+
+//Creates and shows an info iziToast, returning a function that dismisses it
+function makeInfoToast(settings) {
+    var uuid = makeUUID();
+    settings.id = uuid;
+    settings.timeout = false;
+    _izitoast2.default.info(settings);
+
+    var toast = document.getElementById(uuid);
+
+    return function () {
+        _izitoast2.default.hide(toast, {
+            transitionOut: "fadeOutUp"
+        });
+    };
+}
+
+function makeUUID() {
+    // Random string with very little collision possibility
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, function (c) {
+        return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16);
+    });
+}
+
+exports.default = makeInfoToast;
+//# sourceMappingURL=dismissable_toast_maker.js.map
+//# sourceMappingURL=dismissable_toast_maker.js.map
