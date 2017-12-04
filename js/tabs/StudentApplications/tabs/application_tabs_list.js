2a9c3dd2b167e749fdff4f5b2b6c768147bfa35d
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-12-04 17:44:58 +0800

    Added student overview to tabs list

--- js/tabs/StudentApplications/tabs/application_tabs_list.js
+++ js/tabs/StudentApplications/tabs/application_tabs_list.js
@@ -1,9 +1,14 @@
 import React from "react";
+import StudentOverview from "../../Students/tabs/overview";
 
 const tabs = [
     {
         name : "Overview",
-        tab : null,
+        tab : (applicant, refreshStudents) => {
+            return <StudentOverview applicant
+                                    student={ applicant }
+                                    refreshStudents={ refreshStudents }/>;
+        },
         image : "./images/burgergrey.png",
         activeImage : "./images/burgergreen.png",
     },
